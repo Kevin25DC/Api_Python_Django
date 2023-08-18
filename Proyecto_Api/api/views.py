6ce32ea8be7d3058fe316dc19fd57caf39ea974e
kevin25DC <kevinhern40@gmail.com>
2023-08-17 22:53:53 -0500

    se esta haciendo la vita en la carpeta view, y se creo el archivo urls, para convertir la clase la cual se creo en el archivo view

--- Proyecto_Api/api/views.py
+++ Proyecto_Api/api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+#from django.shortcuts import render # esto es para renderizar plantilla
+from django.views import View # esta clase se imorrta para las vistas
 
-# Create your views here.
+#metodos 
+class autorview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
+    
+    def get(self,request): 
+        pass
+
+    def post(self,request):
+        pass
+
+    def put(self,request):
+        pass
+
+    def delete(self,request):
+        pass
+    
+
+class libroview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
+    
+    def get(self,request): 
+        pass
+
+    def post(self,request):
+        pass
+
+    def put(self,request):
+        pass
+
+    def delete(self,request):
+        pass
+
+
+class auto_librview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
+    
+    def get(self,request): 
+        pass
+
+    def post(self,request):
+        pass
+
+    def put(self,request):
+        pass
+
+    def delete(self,request):
+        pass
