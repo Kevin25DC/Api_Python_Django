f55de7bf338e7fc3502c6fa8c9f8245c95d43f94
kevin25DC <kevinhern40@gmail.com>
2023-08-18 19:26:57 -0500

    metodos de get, post, delete, put

--- Proyecto_Api/Proyecto_Api/urls.py
+++ Proyecto_Api/Proyecto_Api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include #se incluye esto en la ruta para establecer como las ur ingresadas por los usuarios se mapearan a la vistas y funcionalidades de la app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Proyecto_Api/api/views.py
+++ Proyecto_Api/api/views.py
@@ -1,13 +1,19 @@
 #from django.shortcuts import render # esto es para renderizar plantilla
+from django.http import JsonResponse
 from django.views import View # esta clase se imorrta para las vistas
-
+from .models import autor #,libro,auto_libr
 #metodos 
 class autorview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
     
-    def get(self,request): 
-        pass
+    def get(request):
+        autores = autor.objects.all()  # Consulta todos los registros del modelo Autor
+        if autores.exists():
+            datos = {'Mensaje': "Exitoso", 'autores': autores}
+        else:
+            datos = {'Mensaje': "autores no encontrados"}
+        return JsonResponse(datos)
 
-    def post(self,request):
+    def post(self,request): 
         pass
 
     def put(self,request):
@@ -20,7 +26,12 @@ class autorview(View): # esta clase se convertira en una vista, que sea capas de
 class libroview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
     
     def get(self,request): 
-        pass
+        libro = libro.objects.all()
+        if libro.exits():
+            datos ={'Mensaje': "exitoso",'libro':libro}
+        else:
+            datos ={'Mensaje':"libro encontrado"}
+        return JsonResponse(datos)
 
     def post(self,request):
         pass
@@ -35,8 +46,12 @@ class libroview(View): # esta clase se convertira en una vista, que sea capas de
 class auto_librview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
     
     def get(self,request): 
-        pass
-
+        auto_librview = auto_librview.objects.all()
+        if auto_librview.exits():
+            datos ={'Mensaje':"Exitoso",'auto_libr':auto_librview}
+        else:
+            datos ={'Mensaje':"auto_libr encontrado"}
+        return JsonResponse(datos)
     def post(self,request):
         pass
 
