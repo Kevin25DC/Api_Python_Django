8aecceb26073c29ba847ef3f4d1f2d6cc59f3bf1
kevin25DC <kevinhern40@gmail.com>
2023-08-19 11:16:17 -0500

    mode bd

--- Proyecto_Api/api/views.py
+++ Proyecto_Api/api/views.py
@@ -1,9 +1,18 @@
 #from django.shortcuts import render # esto es para renderizar plantilla
+from typing import Any
+from django import http
 from django.http import JsonResponse
 from django.views import View # esta clase se imorrta para las vistas
 from .models import autor,libro,auto_libr
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 #metodos 
 class autorview(View): # esta clase se convertira en una vista, que sea capas de procesar las respuesta
+        
+        @method_decorator (csrf_exempt)
+        def dispatch(self, request, *args: Any, **kwargs):  #este metodo es para hacer una peticion, el cual coloque para que no hubiera problema al momento de insertar un datos en la bse de datos desde un Json
+            return super().dispatch(request, *args, **kwargs)
+        
         def get(self, request):
             autores = autor.objects.all()
             autores_serializados = list(autores.values())  # Convertir el QuerySet en una lista de diccionarios
@@ -12,6 +21,23 @@ class autorview(View): # esta clase se convertira en una vista, que sea capas de
             else:
                 datos = {'Mensaje': "autores no encontrados"}
             return JsonResponse(datos)
+        
+        def post(self, request):
+            try:
+        # Aquí realizas la inserción de datos en la base de datos
+        # Por ejemplo, si 'nombre' y 'apellido' son campos de tu modelo 'autor':
+                nombre = request.POST.get('nombre')
+                apellido = request.POST.get('apellido')
+                ciudad = request.POST.get('ciudad')
+                fecha_creacion = request.POST.get('fecha_creacion')
+                nuevo_autor = autor(nombre=nombre, apellido=apellido, ciudad=ciudad,fecha_creacion=fecha_creacion)
+                nuevo_autor.save()
+
+                datos = {'Mensaje': "Datos insertados exitosamente"}
+            except Exception as e:
+                datos = {'Mensaje': "Error al insertar datos"}
+            return JsonResponse(datos)
+
 
    
 
